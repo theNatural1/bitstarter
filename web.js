6ad678d0a67ff060df98c48e205b9d11064110a9
theNatural1 <danielyso@gmail.com>
2013-08-05 10:29:25 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
+var htmlfile = "index.html";
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
   response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 9080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
